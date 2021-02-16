1c521f4d6521d9425fb9b3364ef213f327fcaf74
Damien Valet <62466784+USSIZ@users.noreply.github.com>
2021-02-16 11:28:35 +0100

    Sprint 1.2 BACK - Tryin to fix error in last migration (2)

--- Back/migrations/Version20210216100405.php
+++ Back/migrations/Version20210216100405.php
@@ -20,7 +20,7 @@ final class Version20210216100405 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD role LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', DROP api_token');
+        $this->addSql('ALTER TABLE user ADD role LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\'');
     }
 
     public function down(Schema $schema) : void
