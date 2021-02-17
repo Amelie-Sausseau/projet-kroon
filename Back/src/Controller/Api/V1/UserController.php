245433e6d614bca6a178a8f6ffbf327a797d6c7e
Amelie-Sausseau <the_world_is_mine17@hotmail.fr>
2021-02-17 13:28:56 +0100

    Sprint 1.3 BACK - AuthController deleted + add register function in UserController

--- Back/src/Controller/Api/V1/UserController.php
+++ Back/src/Controller/Api/V1/UserController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * @Route("api/v1/users", name="api_v1_user_")
@@ -33,6 +34,37 @@ class UserController extends AbstractController
         return $this->json($user, 200, [
             'Access-Control-Allow-Origin' => '*'
         ], ['groups' => 'user:one']);
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $encoder, UserRepository $userRepository): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegisterType::class, $user);
+
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Encodage du mot de passe
+            $user->setPassword($encoder->encodePassword($user, $user->getPassword()));
+            // Assignartion du rôle par défaut VIA le nom du rôle et non l'ID
+            //$role = $userRepository->findOneByRoleString('ROLE_USER');
+            //$user->setRole($role);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('api_login');
+        }
+        
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     /**
