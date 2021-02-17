c1658873cc82fcaf84fbf3c7974e88b38c4dd6c5
JordanDUPONT <70507700+JordanDUPONT@users.noreply.github.com>
2021-02-17 14:03:01 +0100

    Sprint 1.3 - FRONT - [modif > yarn start Damien]

--- Front/src/components/HomeUsers/index.js
+++ Front/src/components/HomeUsers/index.js
@@ -7,7 +7,7 @@ import PostCard from 'src/components/PostCard';
 
 import './style.scss';
 
-const HomeUsers = ({ posts }) => (
+const HomeUsers = () => (
   <div>
     <div className="home">
       <h1>Edito</h1>
@@ -15,13 +15,7 @@ const HomeUsers = ({ posts }) => (
         has been the industry's standard dummy text ever since the 1500s, when an unknown printer
         took a galley of type and scrambled it to make a type specimen book.
       </div>
- {/*      {
-     posts.map((data) => (
 
-       <PostCard key={data.id} {...data} />
-     ))
-
-   } */}
       <h2>Derniers sons</h2>
 
       <div className="last_soungs">
