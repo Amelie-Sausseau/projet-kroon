9e87f02224ee6d2243eedefd1c5ee3df96f9402a
Damien Valet <62466784+USSIZ@users.noreply.github.com>
2021-02-19 16:34:33 +0100

    Sprint 1.5 BACK - addEventListener in RegisterType from Mickael (5)

--- Back/src/Form/RegisterType.php
+++ Back/src/Form/RegisterType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
