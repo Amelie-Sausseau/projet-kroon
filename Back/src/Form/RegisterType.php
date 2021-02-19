22a38dd6f4418bed2d492a45347212c1805a24b3
Damien Valet <62466784+USSIZ@users.noreply.github.com>
2021-02-19 16:42:11 +0100

    Sprint 1.5 BACK - addEventListener in RegisterType from Mickael (6)

--- Back/src/Form/RegisterType.php
+++ Back/src/Form/RegisterType.php
@@ -21,7 +21,7 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         // FIXME: Ligne + méthode onPreSetData crées par Mickael => à vérifier si erreur
-        $builder->addEventListener(FormEvents::PRE_SET_DATA, $this->onPreSetData());
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, [ $this, 'onPreSetData' ]);
 
         $builder
             ->add('name', TextType::class, [
