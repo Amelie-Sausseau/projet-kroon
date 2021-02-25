30a8c565a9fb988d7e127014d31dfdbf5c3ee7fe
Amelie-Sausseau <the_world_is_mine17@hotmail.fr>
2021-02-25 13:59:53 +0100

    Sprint 2.4 BACK - Fixing url for insert (3)

---
+++ Back/src/Service/FileUploader.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class FileUploader
+{
+    private $avatarDirectory;
+    private $soundDirectory;
+    private $slugger;
+
+    public function uploadAvatar(UploadedFile $file)
+    {
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $this->slugger->slug($originalFilename);
+        $fileName = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+        try {
+            $file->move($this->getAvatarDirectory(), $fileName);
+        } catch (FileException $e) {
+            // ... handle exception if something happens during file upload
+        }
+
+        return $fileName;
+    }
+
+    public function uploadSound(UploadedFile $file)
+    {
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $this->slugger->slug($originalFilename);
+        $fileName = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+        try {
+            $file->move($this->getSoundDirectory(), $fileName);
+        } catch (FileException $e) {
+            // ... handle exception if something happens during file upload
+        }
+
+        return $fileName;
+    }
+
+    public function getAvatarDirectory()
+    {
+        return $this->avatarDirectory;
+    }
+
+    public function getSoundDirectory()
+    {
+        return $this->soundDirectory;
+    }
+} 
