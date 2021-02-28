4ff8da260120e5a87902e3b86c6c5ff1bbb039ad
Amelie-Sausseau <the_world_is_mine17@hotmail.fr>
2021-02-28 20:48:30 +0100

    Sprint 3.0 BACK - Admin branch creation + easyadmin installation

---
+++ Back/src/Controller/Admin/DashboardController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Entity\Tag;
+use App\Entity\User;
+use App\Repository\CommentRepository;
+use App\Repository\PostRepository;
+use Doctrine\DBAL\Query\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Kroon Admin');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-users', User::class);
+        yield MenuItem::linkToCrud('Publications', 'fa fa-pencil-square-o', Post::class);
+        yield MenuItem::linkToCrud('Commentaires', 'fas fa-comments', Comment::class);
+        yield MenuItem::linkToCrud('Catégories', 'fas fa-tags', Tag::class);
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+
+}
---
+++ Back/src/Controller/Admin/PostCrudController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Post;
+use App\Entity\Tag;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class PostCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Post::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return  [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('title'),
+            TextField::new('body'),
+            //AssociationField::new('tag', 'Tag'),
+            TextField::new('soundFile'),
+            BooleanField::new('isActive'),
+            BooleanField::new('isReported'),
+            BooleanField::new('isSolved'),
+            BooleanField::new('isClosed'),
+            DateTimeField::new('createdAt')->hideOnForm(),
+            DateTimeField::new('updatedAt')->hideOnForm(),
+        ];
+    }
+    
+}
