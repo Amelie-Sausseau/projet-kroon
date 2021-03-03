71ff92437ddb3e832cc89baae52417619a382b2c
JordanDUPONT <70507700+JordanDUPONT@users.noreply.github.com>
2021-03-03 11:17:22 +0100

    sprint 3.3 - front - stylisation comment

--- Front/src/components/Categories/index.js
+++ Front/src/components/Categories/index.js
@@ -1,14 +1,10 @@
-import PropTypes from 'prop-types';
-
 import React, { useEffect, useState } from 'react';
 import ReactAudioPlayer from 'react-audio-player';
-
 import { NavLink } from 'react-router-dom';
-
-
+import SearchBar from 'src/components/SearchBar';
 import './categories.scss';
 
-const Categories = ({ 
+const Categories = ({
   fetchAllPosts,
   fetchCategorie1,
   fetchCategorie2,
@@ -26,137 +22,158 @@ const Categories = ({
     [],
   );
 
-const [Music, setMusic] = useState(false);
-const [Animaux, setAnimaux] = useState(false);
-const [Film, setFilm] = useState(false);
-const [Video, setVideo] = useState(false);
-const [Autres, setAutres] = useState(false);
+  const [Music, setMusic] = useState(false);
+  const [Animaux, setAnimaux] = useState(false);
+  const [Film, setFilm] = useState(false);
+  const [Video, setVideo] = useState(false);
+  const [Autres, setAutres] = useState(false);
 
-function onClickMusic (){
-  setMusic(!Music)
-  fetchCategorie1()
+  function onClickMusic() {
+    setMusic(!Music);
+    fetchCategorie1();
   }
 
-  function onClickAnimaux (){
-    setAnimaux(!Animaux)
-    fetchCategorie2()
-    }
+  function onClickAnimaux() {
+    setAnimaux(!Animaux);
+    fetchCategorie2();
+  }
 
-    function onClickFilm (){
-      setFilm(!Film)
-      fetchCategorie3()
-      }
+  function onClickFilm() {
+    setFilm(!Film);
+    fetchCategorie3();
+  }
 
-      function onClickVideo (){
-        setVideo(!Video)
-        fetchCategorie4()
-        }
+  function onClickVideo() {
+    setVideo(!Video);
+    fetchCategorie4();
+  }
 
-        function onClickAutre (){
-          setAutres(!Autres)
-          fetchCategorie5()
-          }
+  function onClickAutre() {
+    setAutres(!Autres);
+    fetchCategorie5();
+  }
 
-          const publi = "/publicationsUser/"
+  const publi = '/publicationsUser/';
 
-return (
-  <div className="catContainers">
+  return (
+    <>
+      <div className="catContainers">
 
-    <div>SearchBarre O\</div>
-       <div className="UserComments"
-        onClick={onClickMusic}>
-        <h1>Musique</h1> 
-    </div>
-    {
+        <div
+          className="UserComments"
+          onClick={onClickMusic}
+        >
+          <h1>Musique</h1>
+        </div>
+        {
        categorieMusic.map((post) => (
-        Music ? 
-        <NavLink to={publi + post.id} >
-    <div>              
-        <h2>{post.title}</h2>
-        <ReactAudioPlayer
-         src={post.sound} 
-         controls
-          preload="auto"
-         />
-         
-   </div> 
-   </NavLink>: null
+         Music
+           ? (
+             <NavLink to={publi + post.id}>
+               <div>
+                 <h2>{post.title}</h2>
+                 <ReactAudioPlayer
+                   src={post.sound}
+                   controls
+                   preload="auto"
+                 />
+
+               </div>
+             </NavLink>
+           ) : null
        ))
     }
-           <div className="UserComments"
-        onClick={onClickAnimaux}>
-        <h1>Animaux</h1> 
-    </div>
-    {
+        <div
+          className="UserComments"
+          onClick={onClickAnimaux}
+        >
+          <h1>Animaux</h1>
+        </div>
+        {
        categorie2.map((post) => (
-        Animaux ? 
-        <NavLink to={publi + post.id} >
-        <div><h2>{post.title}</h2>
-        <ReactAudioPlayer
-         src={post.sound} 
-         controls
-          preload="auto"
-         />
-         </div>
-         </NavLink> : null
+         Animaux
+           ? (
+             <NavLink to={publi + post.id}>
+               <div><h2>{post.title}</h2>
+                 <ReactAudioPlayer
+                   src={post.sound}
+                   controls
+                   preload="auto"
+                 />
+               </div>
+             </NavLink>
+           ) : null
        ))
     }
-           <div className="UserComments"
-        onClick={onClickFilm}>
-        <h1>Film/Série</h1> 
-    </div>
-    {
+        <div
+          className="UserComments"
+          onClick={onClickFilm}
+        >
+          <h1>Film/Série</h1>
+        </div>
+        {
        categorie3.map((post) => (
-        Film ? 
-        <NavLink to={publi + post.id} >
-        <div><h2>{post.title}</h2>
-        <ReactAudioPlayer
-         src={post.sound} 
-         controls
-          preload="auto"
-         />
-         </div>
-         </NavLink> : null
+         Film
+           ? (
+             <NavLink to={publi + post.id}>
+               <div><h2>{post.title}</h2>
+                 <ReactAudioPlayer
+                   src={post.sound}
+                   controls
+                   preload="auto"
+                 />
+               </div>
+             </NavLink>
+           ) : null
        ))
     }
-           <div className="UserComments"
-        onClick={onClickVideo}>
-        <h1>Vidéos</h1> 
-    </div>
-    {
+        <div
+          className="UserComments"
+          onClick={onClickVideo}
+        >
+          <h1>Vidéos</h1>
+        </div>
+        {
        categorie4.map((post) => (
-        Video ? 
-        <NavLink to={publi + post.id} >
-        <div><h2>{post.title}</h2>
-        <ReactAudioPlayer
-         src={post.sound} 
-         controls
-          preload="auto"
-         />
-         </div>
-         </NavLink> : null
+         Video
+           ? (
+             <NavLink to={publi + post.id}>
+               <div><h2>{post.title}</h2>
+                 <ReactAudioPlayer
+                   src={post.sound}
+                   controls
+                   preload="auto"
+                 />
+               </div>
+             </NavLink>
+           ) : null
        ))
     }
-           <div className="UserComments"
-        onClick={onClickAutre}>
-        <h1>Autres</h1> 
-    </div>
-    {
+        <div
+          className="UserComments"
+          onClick={onClickAutre}
+        >
+          <h1>Autres</h1>
+        </div>
+        {
        categorie5.map((post) => (
-        Autres ? 
-        <NavLink to={publi + post.id} >
-        <div><h2>{post.title}</h2>
-        <ReactAudioPlayer
-         src={post.sound} 
-         controls
-          preload="auto"
-         />
-         </div>
-         </NavLink> : null
+         Autres
+           ? (
+             <NavLink to={publi + post.id}>
+               <div><h2>{post.title}</h2>
+                 <ReactAudioPlayer
+                   src={post.sound}
+                   controls
+                   preload="auto"
+                 />
+               </div>
+             </NavLink>
+           ) : null
        ))
     }
-  </div>
-)};
+      </div>
+    </>
+  );
+};
 
 export default Categories;
-
--- Front/src/components/SearchBar/index.js
+++ Front/src/components/SearchBar/index.js
@@ -1,2 +1,26 @@
-// ENCORE RIEN FAIT PETIT CURIEUX
-// FUCK LES JALOUX
+import React from 'react';
+
+import './searchbar.scss';
+
+const SearchBar = ({ placeholder, handleChange }) => {
+  const formSubmit = (event) => {
+    event.preventDefault();
+    dataSearch();
+  };
+  return (
+    <form action="/" className="formSearch" onSubmit={formSubmit}>
+      <label htmlFor="header-search">
+        <span className="visually-hidden">Rechercher un son</span>
+      </label>
+      <input
+        type="text"
+        id="header-search"
+        placeholder={placeholder}
+        onChange={handleChange}
+      />
+      <button type="submit" className="Search">Rechercher</button>
+    </form>
+  );
+};
+
+export default SearchBar;
